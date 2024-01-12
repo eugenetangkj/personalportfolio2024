a1fe3781acaefa321ed50b9b2e87deebfe0f5a97
eugenetangkj <dragoneugene10@gmail.com>
2024-01-13 01:24:00 +0800

    Add animations

--- components/About.tsx
+++ components/About.tsx
@@ -3,21 +3,21 @@
 import styles from './styles.module.css';
 import Image from 'next/image';
 import { values } from '@/constants';
-import { sourceCodePro } from '@/app/fonts';
-import { Typewriter } from 'nextjs-simple-typewriter';
+import { useInView } from "framer-motion";
+import { useRef } from 'react';
 
-import { motion } from "framer-motion";
-import { useState, useEffect } from 'react';
 
 //Hero section
 function About() {
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: true });
 
     return (
         <section id="about" className='flex flex-col justify-center items-center h-screen space-y-16'>
             <div className='flex flex-col justify-center items-center w-1/2 space-y-16'>
                 <div className='flex flex-col justify-center items-center space-y-4'>
                     <h6 className='font-semibold text-lg uppercase tracking-wide'>About Me</h6>
-                    <h2 className='font-bold text-3xl xs:text-4xl sm:text-5xl lg:text-6xl mx-auto'>It&apos;s all about making an <span className={`${styles.gradientText}`}>impact.</span></h2>
+                    <h2 className='font-bold text-3xl xs:text-4xl sm:text-5xl lg:text-6xl mx-auto' ref={ref}>It&apos;s all about making an <span className={`${styles.gradientText}`}>impact.</span></h2>
                 </div>
                 <p className='text-lg text-center text-dark-gray leading-relaxed'>I am a penultimate Computer Science undergraduate at the National University of Singapore, passionate about leveraging technology to create user-centric digital products with a meaningful impact.</p>
             </div>
@@ -27,7 +27,13 @@ function About() {
                 {
                     values.map((value) => {
                         return (
-                            <div className='flex flex-col justify-center items-center space-y-12'>
+                            <div className='flex flex-col justify-center items-center space-y-12' key={ value.label }
+                                style={{
+                                    opacity: isInView ? 1 : 0,
+                                    transition: `all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) ${ value.delay }`
+        
+                                }}
+                            >
                                 <div className='flex flex-col justify-center items-center space-y-4'>
                                     <Image alt={ value.label } src={ value.imageSource } height={150} width={150} />
                                     <h3 className='text-4xl font-bold'>{ value.label }</h3>
--- constants/index.js
+++ constants/index.js
@@ -21,17 +21,20 @@ export const values = [
     {
         label: "Tech",
         imageSource: "/assets/icons/about-tech.png",
-        content: "I specialize in software engineering and database systems, with experience in web, app and game development."
+        content: "I specialize in software engineering and database systems, with experience in web, app and game development.",
+        delay: "0.5s"
     },
     {
         label: "Design",
         imageSource: "/assets/icons/about-design.png",
-        content: "I prioritize user experience in my products, possessing knowledge about the design life cycle and prototyping skills."
+        content: "I prioritize user experience in my products, possessing knowledge about the design life cycle and prototyping skills.",
+        delay: "1s"
     },
     {
         label: "Community",
         imageSource: "/assets/icons/about-community.png",
-        content: "I actively contribute to my community, using my knowledge to solve problems and help those in need."
+        content: "I actively contribute to my community, using my knowledge to solve problems and help those in need.",
+        delay: "1.5s"
     },
 
 

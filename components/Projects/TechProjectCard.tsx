a61968e1d5973cb994b1bd0610bb14cbe0e2b67d
eugenetangkj <dragoneugene10@gmail.com>
2024-01-13 14:19:37 +0800

    Fix transitions

--- components/Projects/TechProjectCard.tsx
+++ components/Projects/TechProjectCard.tsx
@@ -1,21 +1,41 @@
+"use client";
+
+import { FaAward } from "react-icons/fa";
+import { sourceCodePro } from "@/app/fonts";
+
+import { IoDocumentOutline } from 'react-icons/io5';
+import { FaFigma } from 'react-icons/fa';
+import { IoEarthOutline } from 'react-icons/io5';
 
 function TechProjectCard() {
     return (
-        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="#">
-                <img className="rounded-t-lg" src="/assets/projects/tech/shooting-portals.png" alt="" />
-            </a>
-            <div className="p-5">
-                <a href="#">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                </a>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Read more
-                    <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                    </svg>
-                </a>
+        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-lg" style={{ "height": "600px" }}>
+            <img className="rounded-t-lg" src="/assets/projects/tech/shooting-portals.png" alt="" />
+            
+            <div className="p-5 space-y-4">
+                <h5 className="mb-2 text-2xl font-semibold tracking-tight ">Noteworthy technology acquisitions 2021</h5>
+                <div className='flex flex-row space-x-2 items-center'>
+                    <FaAward className='text-yellow-400 text-2xl' />
+                    <h5 className='text-sm'>Awarded with the Judge's Choice award</h5>
+                </div>
+                
+                <p className="mb-3 font-normal text-dark-gray leading-relaxed">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+
+                <h6 className={`text-label-pink ${ sourceCodePro.className } text-sm md:text-sm`}>Unity, Microsoft Azure PlayFab</h6>
+
+                <div className='grid grid-cols-3 place-items-start'>
+                    <a href='#' target="_blank" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:text-blue-700 duration-300">
+                        Report<IoDocumentOutline className='text-xl ml-2' />  
+                    </a>
+
+                    <a href='#' target="_blank" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:text-blue-700 duration-300">
+                        Report<IoDocumentOutline className='text-xl ml-2' />  
+                    </a>
+
+                    <a href='#' target="_blank" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:text-blue-700 duration-300">
+                        Report<IoDocumentOutline className='text-xl ml-2' />  
+                    </a>
+                </div>  
             </div>
         </div>
     );
